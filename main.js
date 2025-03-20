38853fec3deae081d17eabd92b06ab5c074ee71c
GuilhermeMacario <gui.macario256@gmail.com>
2025-03-20 20:01:54 -0300

    testee

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ function soma(teupai,nu) {
     return sum
 }
 console.log(soma(a,b))
+
+const sub = (a,b) => {
+    return a-b;
+}
+console.log(sub(a,b))
+
+console.log(soma(a,b))
+
+const subb = () => {
+    return 5-3;
+}
+console.log(subb)
