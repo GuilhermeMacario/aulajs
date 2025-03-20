ea3a12ab5d8cc494d061c67e9f5f48c42204a366
GuilhermeMacario <gui.macario256@gmail.com>
2025-03-20 20:46:54 -0300

    teste

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
+const array = [2,3,4,5,6,]; //criando array
+array.forEach (item=>{ // para cada item do array faça tal coisa
+    if (item % 2 === 0) {
+        console.log(`O numero ${item} é par`); // faça isso...
+    }
+    else{//se não for...
+        console.log(`O numero ${item} é impar`); // faça isso...
+    }
 
-var a = 25
-const b = 10
 
-function soma(teupai,nu) {
-    let sum = teupai+nu
-    return sum
-}
-console.log(soma(a,b))
-
-const sub = (a,b) => {
-    return a-b;
-}
-console.log(sub(a,b))
-
-console.log(soma(a,b))
-
-const subb = () => {
-    return 5-3;
-}
-console.log(subb)
+});
