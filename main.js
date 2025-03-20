91c46882f5d1a4a6e8e7c67920debbf9c9dfe100
GuilhermeMacario <gui.macario256@gmail.com>
2025-03-20 20:57:33 -0300

    teste

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const array = [2,3,4,5,6,]; //criando array
-array.forEach (item=>{ // para cada item do array faça tal coisa
-    if (item % 2 === 0) {
-        console.log(`O numero ${item} é par`); // faça isso...
-    }
-    else{//se não for...
-        console.log(`O numero ${item} é impar`); // faça isso...
-    }
+function Pessoa (a,b){
+    this.nome = a;
+    this.idade = b; 
+    
 
+}
+const p = new Pessoa ('Macario',31);
+const y = new Pessoa ('Maria',20);
+console.log (p,y);
 
-});
