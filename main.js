5f460126d540c521e5d4bfb16dcbef6aa6b0ed95
GuilhermeMacario <gui.macario256@gmail.com>
2025-03-20 21:10:34 -0300

    teste

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-function Pessoa (a,b){
-    this.nome = a;
-    this.idade = b; 
+function Carro (a,b,c,d,e){
+    this.cor = a;
+    this.ano = b; 
+    this.modelo = c;
+    this.popular = d;
+    this.valor = e;
     
 
 }
-const p = new Pessoa ('Macario',31);
-const y = new Pessoa ('Maria',20);
-console.log (p,y);
+const g = new Carro ('Vermelho',2001,'hb20',false,20000);
+const h = new Carro ('azul',3101,'gol',true,50000);
 
+console.log (g);
+console.log (h);
